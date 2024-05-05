b15f3bbd703e042b09bb901cd554e8ba4a426d39
Rasmus Stavenuiter <rkst@rd8.tech>
2024-05-05 17:14:33 +0200

    smarty

--- 7-smarty/oxygen.py
+++ 7-smarty/oxygen.py
@@ -1,9 +1,18 @@
-import png
+from imageio.v2 import imread
+import re
 
-reader = png.Reader(filename='smarty/smarty_files/oxygen.png')
 
-chunkit = reader.chunks()
 
-for chunk_type, chunk_data in chunkit:
-    if chunk_type == b'IHDR':
-        print(chunk_data)
+img = imread('7-smarty/smarty_files/oxygen.png')
+data = []
+mid_row = img.shape[0] // 2
+for i in range(img.shape[1]):
+    # print(img[mid_row][i])
+    if img[mid_row][i][0] != img[mid_row][i][1]:
+        break
+    if i % 7 == 0:
+        data.append(chr(img[mid_row][i][0]))
+print(''.join(data))
+
+next_level = re.findall(r'\d+', ''.join(data))
+print(''.join([chr(int(x)) for x in next_level]))
